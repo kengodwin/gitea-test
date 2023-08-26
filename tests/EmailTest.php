615da719df777b9a821d934a4dc7b5cc4c96554d
kengodwin <kengodwin@users.noreply.github.com>
2023-08-26 14:44:15 +0000

    Apply php-cs-fixer changes

--- tests/EmailTest.php
+++ tests/EmailTest.php
@@ -1,6 +1,24 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of PHP CS Fixer.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 use PHPUnit\Framework\TestCase;
 
+/**
+ * @internal
+ *
+ * @coversNothing
+ */
 final class EmailTest extends TestCase
 {
     public function testCanBeCreatedFromValidEmail(): void
@@ -9,7 +27,7 @@ final class EmailTest extends TestCase
 
         $email = Email::fromString($string);
 
-        $this->assertSame($string, $email->asString());
+        self::assertSame($string, $email->asString());
     }
 
     public function testCannotBeCreatedFromInvalidEmail(): void
